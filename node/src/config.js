1a84d6011a950d0eb22b2972bc9c7bae0daaa194
Артем Черенков <art.cherenkov@gmail.com>
2023-10-09 00:03:10 +0300

    отрефакторил репозиторий

--- node/src/config.js
+++
@@ -1,8 +0,0 @@
-import { env } from "node:process";
-
-export const currentRuntime = env.CURRENT_RUNTIME || "unknown";
-export const iterationsCount = env.EXP_ITERATIONS || 10;
-
-export const arraySize = env.ARRAY_SIZE || 100000;
-export const fibCount = env.FIB_COUNT || 30;
-export const asyncTasksCount = env.ASYNC_TASKS_COUNT || 1000;
